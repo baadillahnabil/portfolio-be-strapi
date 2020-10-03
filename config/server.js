27bb8d1f89dd7f82c248dda41b2d5b58868d0e88
baadillahnabil <baadillahnabil@gmail.com>
2020-10-03 21:34:23 +0700

    first setup project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e49881b89d4faba7a30a777923b07274'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6301abba-49ea-4e23-84ab-359d34ba7978'
+};
